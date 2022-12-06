5cdb46412748cd54904f7fb40d622cd8a47baa58
Romack Natividad <romack@vesanique.com.au>
2022-12-06 08:57:18 +0800

    added order by, and prepared constants for other types of query

--- src/MySQLQueryBuilder.php
+++ src/MySQLQueryBuilder.php
@@ -4,6 +4,9 @@ namespace DesignPattern;
 use DesignPattern\SQLQueryBuilder;
 
 define('QUERY_TYPE_SELECT', 'select');
+define('QUERY_TYPE_INSERT', 'insert');
+define('QUERY_TYPE_UPDATE', 'update');
+define('QUERY_TYPE_DELETE', 'delete');
 
 class MySQLQueryBuilder implements SQLQueryBuilder
 {
@@ -16,6 +19,7 @@ class MySQLQueryBuilder implements SQLQueryBuilder
 		$this->query->joins = [];
 		$this->query->type = null;
 		$this->query->where = [];
+		$this->query->orders = [];
 		$this->query->limit = null;
 		$this->query->offset = null;
 	}
@@ -33,6 +37,9 @@ class MySQLQueryBuilder implements SQLQueryBuilder
 
 	public function where($field, $operator, $value)
 	{
+		if (!in_array($this->query->type, [QUERY_TYPE_SELECT, QUERY_TYPE_UPDATE, QUERY_TYPE_DELETE])) {
+			return false;
+		}
 		$filter = "$field {$operator} '{$value}'";
 		array_push($this->query->where, $filter);
 
@@ -55,6 +62,14 @@ class MySQLQueryBuilder implements SQLQueryBuilder
 		return $this;
 	}
 
+	public function orderBy($field, $order = 'ASC')
+	{
+		$order_expression = " {$field} {$order}";
+		array_push($this->query->orders, $order_expression);
+
+		return $this;
+	}
+
 	public function getSQL()
 	{
 		$query = $this->query;
@@ -67,6 +82,11 @@ class MySQLQueryBuilder implements SQLQueryBuilder
 		if (!empty($query->where)) {
 			$sql .= " WHERE ";
 			$sql .= implode(' AND ', $query->where);
+		}
+
+		if (!empty($query->orders)) {
+			$sql .= " ORDER BY ";
+			$sql .= implode(', ', $query->orders);
 		}
 
 		if (!is_null($query->limit)) {
